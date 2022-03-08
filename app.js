0dd8007aa44014703a5b646b3e8c05dde573fb66
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-08 12:47:36 -0800

    set and let state - wins and total equal to 0

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
+const cupButton0 = document.getElementById('button-0');
+const cupButton1 = document.getElementById('button-1');
+const cupButton2 = document.getElementById('button-2');
 
-// let state
+const cupContainer0 = document.getElementById('cup-0');
+const cupContainer1 = document.getElementById('cup-1');
+const cupContainer2 = document.getElementById('cup-2');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
+console.log(cupButton0, cupButton1, cupButton2, cupContainer0, cupContainer1, cupContainer2, winsEl, lossesEl, totalEl);
+
+let winsEl = 0;
+let totalEl = 0;
+
+
