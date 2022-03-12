c0cfede676eb569ebbc526deac4c3017ca4fac26
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-11 17:51:26 -0800

    added two functions but now my wins count isnt working

--- app.js
+++ app.js
@@ -3,48 +3,66 @@ const cupButton0 = document.getElementById('button-0');
 const cupButton1 = document.getElementById('button-1');
 const cupButton2 = document.getElementById('button-2');
 
-const cupContainer0 = document.getElementById('cup-0');
-const cupContainer1 = document.getElementById('cup-1');
-const cupContainer2 = document.getElementById('cup-2');
+const cupContainer0 = document.getElementById('cup0-container');
+const cupContainer1 = document.getElementById('cup1-container');
+const cupContainer2 = document.getElementById('cup2-container');
 
 const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
 const totalEl = document.getElementById('total');
 
-
-
 let winsCount = 0;
 let totalCount = 0;
 
-function userGuess(chosenCup) {
-    cupContainer0.src = './assets/regular-cup.png';
-    cupContainer1.src = './assets/regular-cup.png';
-    cupContainer2.src = './assets/regular-cup.png';
+resetStyles();
+
+function userGuess(chosenCup, correctSpot) {
+    resetStyles();
 
     totalCount++;
 
-    const numberBetweenZeroAndThree = Math.floor(Math.random() * 3);
+    let correctCupEl = document.getElementById(correctSpot + '-container');
+    correctCupEl.src = ('./assets/correct-guess.png');
 
-    if (chosenCup === numberBetweenZeroAndThree) {
+    if (chosenCup === correctSpot) {
         winsCount++;
     }
+    totals();
+}
 
-    const correctCupImgEl = document.getElementById(`cup-${numberBetweenZeroAndThree}`);
-    correctCupImgEl.src = './assets/correct-guess.png';
+cupButton0.addEventListener('click', () => {
+    const correctSpot = getRandomItem();
+    userGuess('cup0-container', correctSpot);
+});
 
+cupButton1.addEventListener('click', () => {
+    const correctSpot = getRandomItem();
+    userGuess('cup1-container', correctSpot);
+});
+
+cupButton2.addEventListener('click', () => {
+    const correctSpot = getRandomItem();
+    userGuess('cup2-container', correctSpot);
+});
+
+function resetStyles() {
+    cupContainer0.src = './assets/regular-cup.png';
+    cupContainer1.src = './assets/regular-cup.png';
+    cupContainer2.src = './assets/regular-cup.png';
+}
+
+function getRandomItem() {
+    const chosenItem = [
+        'cup0',
+        'cup1',
+        'cup2'
+    ];
+    const index = Math.floor(Math.random() * chosenItem.length);
+    return chosenItem[index];
+}
+
+function totals() {
     winsEl.textContent = winsCount;
     totalEl.textContent = totalCount;
     lossesEl.textContent = totalCount - winsCount;
 }
-
-cupButton0.addEventListener('click', () => {
-    userGuess(0);
-});
-
-cupButton1.addEventListener('click', () => {
-    userGuess(1);
-});
-
-cupButton2.addEventListener('click', () => {
-    userGuess(2);
-});
